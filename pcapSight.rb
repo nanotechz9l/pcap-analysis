fade274a1ed8a1b23f0453c6e40785a12c387861
root <0xnanoquetz9l@gmail.com>
2014-04-03 17:19:02 -0700

    updates

--- pcapSight.rb
+++ pcapSight.rb
@@ -8,10 +8,10 @@ require 'packetfu'; require 'pcap_tools'; require 'rainbow'
 # See what else from a malware perspective makes sense to add as new features
 
   pcap         = ARGV[0] || exit # User supplies .pcap as an argument or app exits
-  pcapd        = PacketFu::Read.f2a(:file => pcap) # alias for file_to_array
   total_count  = 0
   icmp_count   = 0
   tcp_count    = 0
+	ip_count     = 0
 	udp_count    = 0
   arp_count    = 0
 	ftp_count    = 0
@@ -21,32 +21,40 @@ require 'packetfu'; require 'pcap_tools'; require 'rainbow'
 
 
   ### Process the supplied .pcap
+	pcapd = PacketFu::Read.f2a(:file => pcap) # alias for file_to_array
   pcapd.each do |pkt|
   v = PacketFu::Packet.parse(pkt)
+
+#  v = PacketFu::PcapFile.read_packets(pcapd) do |pkt|
+
+  #puts v[2].proto
 
      # Begin packet count
      total_count += 1
      if v.is_icmp?
-      icmp_count += 1
+        icmp_count += 1
      elsif v.is_tcp?
-      tcp_count += 1
-     elsif v.is_udp?
-      udp_count += 1
+        tcp_count += 1
+     elsif v.is_ip?
+        ip_count += 1
+		 elsif v.is_udp?
+        udp_count += 1
 	   elsif v.is_arp?
-      arp_count += 1
+        arp_count += 1
 	   elsif v.is_ftp?
-      ftp_count += 1
+        ftp_count += 1
      elsif v.is_http?
-      http_count += 1
-	   elsif v.is_dns?
-      dns_count += 1
-	   else v.is_dhcp?
-      dhcp_count += 1
-		 end
-	end
+        http_count += 1
+		 elsif v.is_dhcp?
+        dhcp_count += 1
+	   #elsif v.is_dns?
+       #dns_count += 1
+
 
   # Read packets
   r = PacketFu::PcapFile.read(pcap)
+  pcapd.each do |pkt|
+  v = PacketFu::Packet.parse(pkt)
 
   # Print main banner
   title = "[+] Packet attributes in '#{pcap}'".foreground(:white).bright
@@ -56,11 +64,13 @@ require 'packetfu'; require 'pcap_tools'; require 'rainbow'
 
   # Print total packet count
   puts "Total Packets: ".ljust(20).foreground(:magenta).bright + "#{total_count}".foreground(:magenta).bright
+  #puts proto! "Total Packets: ".ljust(20).foreground(:magenta).bright #+ "#{v}".proto.foreground(:magenta).bright
 
   # Print packet details
   puts "File Size: ".ljust(20).foreground(:magenta).bright + "#{r}".size.to_s.foreground(:magenta).bright + " bytes".foreground(:magenta).bright
   puts "TCP Packets: ".ljust(20).foreground(:magenta).bright + "#{tcp_count}".foreground(:magenta).bright
-  puts "ICMP Packets: ".ljust(20).foreground(:magenta).bright + "#{icmp_count}".foreground(:magenta).bright
+  puts "IP Packets: ".ljust(20).foreground(:magenta).bright + "#{ip_count}".foreground(:magenta).bright
+	puts "ICMP Packets: ".ljust(20).foreground(:magenta).bright + "#{icmp_count}".foreground(:magenta).bright
   puts "UDP Packets: ".ljust(20).foreground(:magenta).bright + "#{udp_count}".foreground(:magenta).bright
   puts "ARP Packets: ".ljust(20).foreground(:magenta).bright + "#{arp_count}".foreground(:magenta).bright
   puts "FTP Packets: ".ljust(20).foreground(:magenta).bright + "#{ftp_count}".foreground(:magenta).bright
@@ -70,16 +80,20 @@ require 'packetfu'; require 'pcap_tools'; require 'rainbow'
 
 
 
-  ### Print TCP banner
+  ### TCP banner
   title = "[+] TCP Protocol Breakdown '#{pcap}'".foreground(:white).bright
   puts "-" * title.size
   puts title
   puts "-" * title.size
 
-  # Print TCP details to user
+  # TCP details to user
+  puts "Total connections:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
+  puts "TCP closed:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
+  puts "TCP open:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
+  puts "TCP filtered:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
   puts "Packets:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
   
-	# Read Source IP's
+  # Read Source IP's
   #src = PacketFu::IPHeader.ip_saddr(pcap)
   #puts src
 	puts "IP Src's:".ljust(20).foreground(:magenta).bright + "#{udp_count}".rjust(25).foreground(:magenta).bright
@@ -89,9 +103,6 @@ require 'packetfu'; require 'pcap_tools'; require 'rainbow'
   #puts dst
 	puts "IP Dst's:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
 
-	# Popular IP's
-	puts "Popular IP Dst's:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
-  puts "Src IP, Dst IP:".ljust(20).foreground(:magenta).bright + "#{tcp_count}".rjust(25).foreground(:magenta).bright
   
 
   ### Print ICMP banner
@@ -100,16 +111,14 @@ require 'packetfu'; require 'pcap_tools'; require 'rainbow'
   puts title
   puts "-" * title.size
 
-	pcapd.each do |pkt|
-  v = PacketFu::Packet.parse(pkt)
 
   # Print ICMP details to user
   puts "Packets:".ljust(20).foreground(:magenta).bright + "#{icmp_count}".rjust(25).foreground(:magenta).bright
   #system `tshark -r | grep ICMP >> one.txt` << "#{v}" 
- # exec('tshark -r "#{v}"')  # The file "#{v}" doesn't exist.
-  %x( tshark -r #{v})
+  # exec('tshark -r "#{v}"')  # The file "#{v}" doesn't exist.
+  #%x( tshark -r #{v})
 
-	puts "IP Dst's:".ljust(20).foreground(:magenta).bright + "#{icmp_count}".rjust(25).foreground(:magenta).bright
+  puts "IP Dst's:".ljust(20).foreground(:magenta).bright + "#{icmp_count}".rjust(25).foreground(:magenta).bright
   puts "Popular IP Dst's:".ljust(20).foreground(:magenta).bright + "#{icmp_count}".rjust(25).foreground(:magenta).bright
   puts "Src IP, Dst IP:".ljust(20).foreground(:magenta).bright + "#{icmp_count}".rjust(25).foreground(:magenta).bright
-	end
+	
